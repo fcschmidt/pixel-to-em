018e4c7d898c66ab160dbd08981b8dd00dc83cbe
fcschmidt <fcschimidt@gmail.com>
2017-07-07 14:58:23 -0300

    Create Service Worker file

---
+++ assets/js/service-worker.js
@@ -0,0 +1,14 @@
+var files = [
+    "index.html",
+    "manifest.json",
+    "assets/css/main.cs",
+    "assets/js/main.js",
+    "assets/service-worker.js",
+    "assets/vendor/jquery.min.js",
+    "assets/vendor/materialize/css/materialize.min.css",
+    "assets/vendor/materialize/js/materialize.min.js",
+    "assets/vendor/materialize/icons/material.css",
+    "assets/vendor/materialize/icons/MaterialIcons-Regular.ttf",
+    "assets/vendor/waves/css/waves.min.css",
+    "assets/vendor/waves/js/waves.min.js"
+];
