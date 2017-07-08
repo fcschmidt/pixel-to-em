f1615c7a4f3a516740bda4215b2c73c1ac1c72ba
fcschmidt <fcschimidt@gmail.com>
2017-07-08 14:49:42 -0300

    Delete and rename archive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // Register the service worker if available.
-if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || window.location.hostname === 'localhost')) {
+if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js').then(function(reg) {
         console.log('Successfully registered service worker', reg);
     }).catch(function(err) {
