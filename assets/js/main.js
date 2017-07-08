84f7385948798ff49bd45cfbd5c90f080dfae77c
fcschmidt <fcschimidt@gmail.com>
2017-07-08 18:30:41 -0300

    Removing redundace and Add to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,15 @@
+$(document).ready(function(){
+    $(document).ready(function() {
+        $('select').material_select();
+        // $('select').material_select('destroy');
+    });
+    $(document).ready(function() {
+        Materialize.updateTextFields();
+    });
+    $(".button-collapse").sideNav();
+    $(document).ready(function(){
+        $('ul.tabs').tabs('select_tab', 'tab_id');
+    });
+});
 
 
