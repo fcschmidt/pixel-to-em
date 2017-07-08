292518dbe60bc44383b5e07291f8cdcec6205b7f
fcschmidt <fcschimidt@gmail.com>
2017-07-08 17:50:01 -0300

    Update ServiceWorker - new test

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,58 +1,56 @@
-// Use a cacheName for cache versioning
-var cacheName = 'v1:static';
+var cacheName = 'v1';
 
-// During the installation phase, you'll usually want to cache static assets.
 // Install the ServiceWorker
-self.addEventListener('install', function(e) {
-    // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
-    e.waitUntil(
+self.addEventListener('install', function(event) {
+    event.waitUntil(
         // Open a cache
         caches.open(cacheName).then(function(cache) {
             // Define  what we want to cache
             return cache.addAll([
                 '/',
-                "index.html",
-                "manifest.json",
-                "assets/css/main.cs",
-                "assets/js/main.js",
-                "assets/js/install.js",
-                "assets/image/icons/launcher-icon.png",
-                "assets/image/icons/launcher-icon-96.png",
-                "assets/image/icons/launcher-icon-144.png",
-                "assets/image/icons/launcher-icon-192.png",
-                "assets/image/icons/launcher-icon-256.png",
-                "assets/vendor/jquery.min.js",
-                "assets/vendor/materialize/css/materialize.min.css",
-                "assets/vendor/materialize/js/materialize.min.js",
-                "assets/vendor/materialize/icons/material.css",
-                "assets/vendor/materialize/icons/MaterialIcons-Regular.ttf",
-                "assets/vendor/waves/css/waves.min.css",
-                "assets/vendor/waves/js/waves.min.js",
-                "assets/vendor/font-awesome/css/font-awesome.min.css",
-                "offline.html"
-            ]).then(function() {
-                self.skipWaiting();
-            });
+                '/index.html',
+                '/manifest.json',
+                '/assets/images/icons/favicon.opng',
+                '/assets/css/main.css',
+                '/assets/js/main.js',
+                '/assets/js/app.js',
+                '/assets/images/icons/icon-48.png',
+                // '/assets/images/icons/icon-96.png',
+                '/assets/images/icons/icon-144.png',
+                '/assets/images/icons/icon-192.png',
+                // '/assets/images/icons/icon-256.png',
+                '/assets/vendor/materialize/css/materialize.min.css',
+                '/assets/vendor/materialize/js/materialize.min.js',
+                '/assets/vendor/materialize/icons/material.css',
+                '/assets/vendor/materialize/icons/MaterialIcons-Regular.ttf',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Bold.woff',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Bold.woff2',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Light.woff',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Light.woff2',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Medium.woff',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Medium.woff2',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Regular.woff',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Regular.woff2',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Thin.woff',
+                '/assets/vendor/materialize/fonts/roboto/Roboto-Thin.woff2',
+                '/assets/vendor/font-awesome/css/font-awesome.min.css',
+                '/assets/vendor/waves/css/waves.min.css',
+                '/assets/vendor/waves/js/waves.min.js',
+                '/assets/vendor/jquery/jquery.min.js'
+                // '/offline.html'
+            ]);
         })
     );
 });
 
-// when the browser fetches a URL…
+
 self.addEventListener('fetch', function(event) {
-    // … either respond with the cached object or go ahead and fetch the actual URL
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) {
-                // retrieve from cache
-                return response;
-            }
-            // fetch as normal
-            return fetch(event.request);
-        })
-    );
-});
-
-self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event);
-  clients.openWindow('/');
+    event.respondWith(caches.match(event.request).catch(function() {
+        return fetch(event.request);
+    }).then(function(response) {
+        caches.open(cacheName).then(function(cache) {
+            cache.put(event.request, response);
+        });
+        return response.clone();
+    })
 });
