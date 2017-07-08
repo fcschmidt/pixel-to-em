836534c0015c0a4b8256f6474b4de8f18b53ffe5
fcschmidt <fcschimidt@gmail.com>
2017-07-08 17:48:52 -0300

    Remove Scope

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,20 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('/assets/js/sw.js', {scope: '/assets/js/sw.js'})
+    navigator.serviceWorker.register('/assets/js/sw.js')
     .then(function(reg) {
 
         if(reg.installing) {
-            console.log('Service worker installing ' + reg.scope);
+            console.log('Service worker installing!');
         } else if(reg.waiting) {
-            console.log('Service worker installed ' + reg.scope);
+            console.log('Service worker installed!');
         } else if(reg.active) {
-            console.log('Service worker active at Scope is ' + reg.scope);
+            console.log('Service worker active!');
         }
     }).catch(function(error) {
         // Registration failed
         console.log('Registration failed with ' + error);
     });
+} else {
+    console.log("Your browser does not supports serviceWorker!");
 }
