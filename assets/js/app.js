026769c4ddb29ceab97990c2e15697d59e20c61e
fcschmidt <fcschimidt@gmail.com>
2017-07-08 15:46:33 -0300

    Update ServiceWorkerCC Registration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,18 @@
-// Register the service worker if available.
+// Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(function(reg) {
-        console.log('Successfully registered service worker', reg);
-    }).catch(function(err) {
-        console.warn('Error whilst registering service worker', err);
+
+    navigator.serviceWorker.register('/assets/js/sw.js', {scope: '/assets/js/sw.js'})
+    .then(function(reg) {
+
+        if(reg.installing) {
+            console.log('Service worker installing ' + reg.scope);
+        } else if(reg.waiting) {
+            console.log('Service worker installed ' + reg.scope);
+        } else if(reg.active) {
+            console.log('Service worker active at Scope is ' + reg.scope);
+        }
+    }).catch(function(error) {
+        // Registration failed
+        console.log('Registration failed with ' + error);
     });
 }
