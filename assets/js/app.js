dabfd7814dc5bfd5e8381da59195b05b1c5e5610
fcschmidt <fcschimidt@gmail.com>
2017-07-08 22:46:44 -0300

    Commit Register Test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
 // Register the service worker
-if ('serviceWorker' in navigator) {
+// if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('assets/js/sw.js')
-    .then(function(reg) {
+//     navigator.serviceWorker.register('assets/js/sw.js')
+//     .then(function(reg) {
 
-        if(reg.installing) {
-            console.log('Service worker installing!');
-        } else if(reg.waiting) {
-            console.log('Service worker installed!');
-        } else if(reg.active) {
-            console.log('Service worker active!');
-        }
-    }).catch(function(error) {
-        // Registration failed
-        console.log('Registration failed with ' + error);
-    });
-} else {
-    console.log("Your browser does not supports serviceWorker!");
-}
+//         if(reg.installing) {
+//             console.log('Service worker installing!');
+//         } else if(reg.waiting) {
+//             console.log('Service worker installed!');
+//         } else if(reg.active) {
+//             console.log('Service worker active!');
+//         }
+//     }).catch(function(error) {
+//         // Registration failed
+//         console.log('Registration failed with ' + error);
+//     });
+// } else {
+//     console.log("Your browser does not supports serviceWorker!");
+// }
