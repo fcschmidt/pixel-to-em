48d570260c131a9bfacc2983977fc1089483511a
fcschmidt <fcschimidt@gmail.com>
2017-07-11 18:19:59 -0300

    Solved http error 404

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,19 @@
 // Register the service worker
-// if ('serviceWorker' in navigator) {
-
-//     navigator.serviceWorker.register('assets/js/sw.js')
-//     .then(function(reg) {
-
-//         if(reg.installing) {
-//             console.log('Service worker installing!');
-//         } else if(reg.waiting) {
-//             console.log('Service worker installed!');
-//         } else if(reg.active) {
-//             console.log('Service worker active!');
-//         }
-//     }).catch(function(error) {
-//         // Registration failed
-//         console.log('Registration failed with ' + error);
-//     });
-// } else {
-//     console.log("Your browser does not supports serviceWorker!");
-// }
+if ('serviceWorker' in navigator) {
+  
+    navigator.serviceWorker.register('service-worker.js',{
+        scope: './'
+    }).then(function(reg) {
+        if(reg.installing) {
+            console.log('Service worker installing!');
+        } else if(reg.waiting) {
+            console.log('Service worker installed!');
+        } else if(reg.active) {
+            console.log('Service worker active!');
+        }
+    }).catch(function(error) {
+        console.log('Registration failed with ' + error);
+    });
+} else {
+    console.log("Your browser does not supports serviceWorker!");
+}
