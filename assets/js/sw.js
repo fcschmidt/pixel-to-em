1f1f4383f5093e6789cda8521082847e2e42166e
fcschmidt <fcschimidt@gmail.com>
2017-07-11 18:16:24 -0300

    Uptade SW

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -2,6 +2,7 @@ var cacheName = 'v1';
 
 // Install the ServiceWorker
 self.addEventListener('install', function(event) {
+    console.log('[SW] Install');
     event.waitUntil(
         // Open a cache
         caches.open(cacheName).then(function(cache) {
@@ -13,15 +14,13 @@ self.addEventListener('install', function(event) {
                 'calc.html',
                 'featured.html',
                 'manifest.json',
-                'assets/images/icons/favicon.opng',
                 'assets/css/main.css',
                 'assets/js/main.js',
                 'assets/js/app.js',
+                'assets/images/icons/favicon.png',
                 'assets/images/icons/icon-48.png',
-                // '/assets/images/icons/icon-96.png',
                 'assets/images/icons/icon-144.png',
                 'assets/images/icons/icon-192.png',
-                // '/assets/images/icons/icon-256.png',
                 'assets/vendor/materialize/css/materialize.min.css',
                 'assets/vendor/materialize/js/materialize.min.js',
                 'assets/vendor/materialize/icons/material.css',
@@ -36,29 +35,71 @@ self.addEventListener('install', function(event) {
 });
 
 
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(caches.match(event.request).catch(function() {
-//         return fetch(event.request);
-//     }).then(function(response) {
-//         caches.open(cacheName).then(function(cache) {
-//             cache.put(event.request, response);
-//         });
-//         return response.clone();
-//     }).catch(function() {
-//         return caches.match('offline.html');
-//     }));
-// });
+self.addEventListener('fetch', function(event){
+    // console.log('[SW] fetch ' + event.request.url)
+    event.respondWith(
+        caches.match(event.request).then(function(response){
+            return response || fetch(event.request).then(function(response){
+                return caches.open(cacheName).then(function(cache){
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        }).catch(function(){
+            return caches.match('offline.html');
+        })
+    );
+});
 
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('install', function(event){
+    // console.log('[SW] Install');
+    event.waitUntil(
+        caches.open('v2').then(function(cache){
+            return cache.addAll([
+                '/',
+                'index.html',
+                'offline.html',
+                'calc.html',
+                'featured.html',
+                'manifest.json',
+                'assets/css/main.css',
+                'assets/js/main.js',
+                'assets/js/app.js',
+                'assets/images/icons/favicon.png',
+                'assets/images/icons/icon-48.png',
+                'assets/images/icons/icon-144.png',
+                'assets/images/icons/icon-192.png',
+                'assets/vendor/materialize/css/materialize.min.css',
+                'assets/vendor/materialize/js/materialize.min.js',
+                'assets/vendor/materialize/icons/material.css',
+                'assets/vendor/materialize/icons/MaterialIcons-Regular.ttf',
+                'assets/vendor/font-awesome/css/font-awesome.min.css',
+                'assets/vendor/waves/css/waves.min.css',
+                'assets/vendor/waves/js/waves.min.js',
+                'assets/vendor/jquery/jquery.min.js'
+            ]);
+        })
+    );
+});
 
-  event.respondWith(
-    // Look for something in the cache that matches the request
-    caches.match(event.request).then(function(response) {
+self.addEventListener('active', function(event){
+    // console.log('[SW] Activate');
+    var cachWhitelist = ['v2'];
 
-      // If we find something, return it
-      // Otherwise, use the network instead
-      return response || fetch(event.request);
-    })
-  );
+    event.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if (cacheWhitelist.indexOf(key) === -1){
+                    // console.log('[SW] Delete cache:', cacheName);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('notificationclick', function(event){
+    console.log('On notification click: ', event);
+    clients.openWindow('/');
 });
