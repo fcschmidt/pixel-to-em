4d7281120db27a68a4283f85793ee2dbeb7bba27
fcschmidt <fcschimidt@gmail.com>
2017-07-20 17:29:18 -0300

    New test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,27 @@
 // Register the service worker
 // Offline Tests
 // if ('serviceWorker' in navigator && (window.location.protocol == 'https:' || window.location.hostname === 'localhost')) {
+// if ('serviceWorker' in navigator) {
+
+//     navigator.serviceWorker.register('service-worker.js',{
+//         scope: './'
+//     }).then(function(reg) {
+//         if(reg.installing) {
+//             console.log('Service worker installing!');
+//         } else if(reg.waiting) {
+//             console.log('Service worker installed!');
+//         } else if(reg.active) {
+//             console.log('Service worker active!', reg);
+//         }
+//     }).catch(function(error) {
+//         console.log('Registration failed with ' + error);
+//     });
+// }
+
 if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('service-worker.js',{
-        scope: ''
-    }).then(function(reg) {
+    navigator.serviceWorker.register('service-worker.js')
+    .then(function(reg) {
         if(reg.installing) {
             console.log('Service worker installing!');
         } else if(reg.waiting) {
