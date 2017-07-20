02ec935032e3ad971d4ddbef9c90d762d938140a
fcschmidt <fcschimidt@gmail.com>
2017-07-20 16:51:27 -0300

    Update Scope

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@
 if ('serviceWorker' in navigator) {
 
     navigator.serviceWorker.register('service-worker.js',{
-        scope: './'
+        scope: '/'
     }).then(function(reg) {
         if(reg.installing) {
             console.log('Service worker installing!');
