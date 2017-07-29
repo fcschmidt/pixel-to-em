8a6f3c1856ba7eb2db04dc5f09b661cd680645f0
fcschmidt <fcschimidt@gmail.com>
2017-07-28 22:32:05 -0300

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
 // Register the service worker
 // if ('serviceWorker' in navigator) {
 
-//     navigator.serviceWorker.register('service-worker.js',{
-//         scope: './'
-//     }).then(function(reg) {
+//     navigator.serviceWorker.register('service-worker.js')
+//     .then(function(reg) {
 //         if(reg.installing) {
 //             console.log('Service worker installing!');
 //         } else if(reg.waiting) {
@@ -16,18 +15,15 @@
 //     });
 // }
 
-if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('service-worker.js')
-    .then(function(reg) {
-        if(reg.installing) {
-            console.log('Service worker installing!');
-        } else if(reg.waiting) {
-            console.log('Service worker installed!');
-        } else if(reg.active) {
-            console.log('Service worker active!', reg);
-        }
-    }).catch(function(error) {
-        console.log('Registration failed with ' + error);
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('service-worker.js').then(function(reg) {
+            // Registration was successful
+            console.log("ServiceWorker registration successful with scope: ", reg.scope);
+        }, function(error){
+            // Registration failed
+            console.log('ServiceWorker registration failed: ', error);
+        });
     });
 }
